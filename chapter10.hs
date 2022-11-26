d0fec23d41b68551a25090322661cbf9c4dfad15
Kotaro Abe <abekoh.bcky@gmail.com>
2022-11-26 16:09:36 +0900

    chapter10

--- chapter10.hs
+++ chapter10.hs
@@ -10,9 +10,48 @@ solveRPN = head . foldl foldingFunction [] . words
     foldingFunction xs "sum" = [sum xs]
     foldingFunction xs numberString = read numberString : xs
 
+data Section = Section {getA :: Int, getB :: Int, getC :: Int}
+  deriving (Show)
+
+type RoadSystem = [Section]
+
+heathrowToLondon :: RoadSystem
+heathrowToLondon = [Section 50 10 30, Section 5 90 20, Section 40 2 25, Section 10 8 0]
+
+data Label = A | B | C deriving (Show)
+
+type Path = [(Label, Int)]
+
+roadStep :: (Path, Path) -> Section -> (Path, Path)
+roadStep (pathA, pathB) (Section a b c) =
+  let timeA = sum (map snd pathA)
+      timeB = sum (map snd pathB)
+      forwardTimeToA = timeA + a
+      crossTimeToA = timeB + b + c
+      forwardTimeToB = timeB + b
+      crossTimeToB = timeA + a + c
+      newPathToA =
+        if forwardTimeToA <= crossTimeToA
+          then (A, a) : pathA
+          else (C, c) : (B, b) : pathB
+      newPathToB =
+        if forwardTimeToB <= crossTimeToB
+          then (B, b) : pathB
+          else (C, c) : (A, a) : pathA
+   in (newPathToA, newPathToB)
+
+optimalPath :: RoadSystem -> Path
+optimalPath roadSystem =
+  let (bestAPath, bestBPath) = foldl roadStep ([], []) roadSystem
+   in if sum (map snd bestAPath) <= sum (map snd bestBPath)
+        then reverse bestAPath
+        else reverse bestBPath
+
 main :: IO ()
 main = do
   print $ solveRPN "10 4 3 + 2 * -"
   print $ solveRPN "2 3.5 +"
   print $ solveRPN "2.7 ln"
   print $ solveRPN "10 10 10 10 sum 4 /"
+  print $ roadStep ([], []) (head heathrowToLondon)
+  print $ optimalPath heathrowToLondon
